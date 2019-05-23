b6883c95abd8f4926ee84424b8b451a2bdced723
Andrew Wooldridge <awooldridge@ebay.com>
2019-05-23 00:37:10 -0700

    Adding content

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let name;
+	import Counter from './Counter.svelte';
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+
+<Counter/>
---
+++ src/Counter.svelte
@@ -0,0 +1,24 @@
+<script>
+    let count = 0;
+
+    function increment() {
+        count += 1;
+    }
+
+    function decrement(){
+        count -= 1;
+    }
+</script>
+
+{#if count > 10}
+    <p>⚠️ That's a little much, don't you think!</p>
+{:else if count < -10}
+     <p>👻 How low can you go!</p>
+{/if}
+<div>Count: {count}</div>
+
+<div>Current count: <input bind:value={count} type="number" /></div>
+
+
+<button on:click={increment}>Increment</button>
+<button on:click={decrement}>Decrement</button>
